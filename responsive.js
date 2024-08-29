a4f844c0118eccb5577bc0bfd667bf99e3fb963b
sajak <sajakbasnet77@gmail.com>
2024-08-29 16:52:21 +0545

    Added something cool

---
+++ responsive.js
@@ -0,0 +1,17 @@
+function updateMessage() {
+    const messageDiv = document.getElementById('message');
+    mainDiv = document.getElementById('main');
+    if (window.innerWidth < 768) {
+        if(mainDiv) {
+            mainDiv.innerHTML = '';
+        }
+        messageDiv.textContent = 'कृपया यो साइट मोबाइलमा नखोल्नुहोस् यो Responsive छैन'
+    } else {
+        mainDiv.innerHTML  = `<img src="./idPhoto.jpg" alt="Lovely😘">`
+
+    }
+}
+
+updateMessage();
+
+window.addEventListener('resize', updateMessage);
